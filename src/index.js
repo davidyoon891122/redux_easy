2310ceb44775caf67390f372e0eb21d9f4c69019
davidyoon <qqpo12@naver.com>
2022-07-27 22:11:07 +0900

    Set a redux
    * Added setting codes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 리덕스를 위한 코드들
+import { createStore } from "redux"
+import rootReducer from "./store"
+import { Provider } from "react-redux"
+
+const store = createStore(rootReducer)
+console.log(store.getState())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
