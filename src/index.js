1b3518168c76d35778e05570e35c65b0b2274e41
davidyoon <qqpo12@naver.com>
2022-07-27 22:21:06 +0900

    Apply Chrome extension redux tools
    * Added codes for extensions

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import { createStore } from "redux"
 import rootReducer from "./store"
 import { Provider } from "react-redux"
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
